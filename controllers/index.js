5ab7f54156ae5a86781ae60578c07fe4206da842
[Candice Weaver] <[candicenw@hotmail.co.uk]>
2022-11-03 10:27:45 +0000

    refactoring

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const passport = require('passport');
 
 module.exports = {
   async postRegister(req, res, next) {
@@ -12,5 +13,19 @@ module.exports = {
     await User.register(newUser, req.body.password);
 
     res.redirect('/');
+  },
+
+  postLogin(req, res, next) {
+    passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/login',
+    })(req, res, next);
+  },
+
+  getLogout(req, res, next) {
+    req.logout(err => {
+      if (err) return next(err);
+      res.redirect('/');
+    });
   },
 };
